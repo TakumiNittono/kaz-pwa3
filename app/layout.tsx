c515353f930f09f2bb0cbef3aac9b852977ca6f6
TakumiNittono <nittonotakumi@gmail.com>
2025-11-30 21:35:33 -0500

    Add beautiful PWA icons with gift box and notification bell design

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,8 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/icon-512x512.png" />
       </head>
       <body>
         <OneSignalProvider>{children}</OneSignalProvider>
